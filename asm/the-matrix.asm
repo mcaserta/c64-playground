0f317b8577bfb926d4425cc8b3ac1ae4f5557cf3
Mirko Caserta <mirko.caserta@gmail.com>
2021-05-06 21:51:27 +0200

    color generation avoids black and white

--- asm/the-matrix.asm
+++ asm/the-matrix.asm
@@ -4,13 +4,14 @@
 
 *=      $1000                   ; starting memory location
 
+black           = $00           ; the color black
+white           = $01           ; the color white
 chclrscr        = $93           ; clear screen
 
 !addr   zpfb     = $fb          ; zero page address $fb
 !addr   zpfc     = $fc          ; zero page address $fc
 !addr   zpfd     = $fd          ; zero page address $fd
 !addr   zpfe     = $fe          ; zero page address $fe
-!addr   chrcolor = $0268        ; set character color
 !addr   scrmem   = $0400        ; start of mapped screen character memory
 !addr   scrmode  = $d011        ; screen mode
 !addr   rasterln = $d012        ; current raster line
@@ -25,17 +26,19 @@ chclrscr        = $93           ; clear screen
 !addr   sidnsval = $d41b        ; SID noise random value
 !addr   colorram = $d800        ; start of mapped screen color memory
 
+.init                           ; initialization
         jsr .sids               ; init SID for pseudo random number generation
-        lda     #$00            ; set 0 (black)
+        lda     #black          ; set black
         sta     bordercl        ; as border color
-.strt
-        jsr     .rand           ; set random number
+
+.strt                           ; start of outer loop location
+        jsr     .prcg           ; set random color
         sta     bordrcl1        ; as char background color 1
-        jsr     .rand           ; set random number
+        jsr     .prcg           ; set random color
         sta     bordrcl2        ; as char background color 2
-        jsr     .rand           ; set random number
+        jsr     .prcg           ; set random color
         sta     bordrcl3        ; as char background color 3
-        jsr     .rand           ; set random number
+        jsr     .prcg           ; set random color
         sta     bordrcl4        ; as char background color 4
         lda     scrmode         ; load screen mode configuration in accumulator
         ora     #$40            ; set the extended color mode flag
@@ -51,11 +54,12 @@ chclrscr        = $93           ; clear screen
         lda     #$d8
         sta     zpfe
 
-.loop   ; label for loop location
-        jsr     .rand           ; load pseudo random number in register A
+.loop                           ; start of inner loop location
+        jsr     .prng           ; load pseudo random number in register A
         tay                     ; transfer value of register A to register Y
-        jsr     .rand           ; load pseudo random number in register A
+        jsr     .prng           ; load pseudo random number in register A
         sta     (zpfb),y        ; write random character in raw screen memory at 
+        jsr     .prcg           ; load pseudo random color in register A
         sta     (zpfd),y        ; write random color in raw screen memory at 
         inc     zpfc            ; increment char memory page
         inc     zpfe            ; increment color memory page
@@ -63,7 +67,7 @@ chclrscr        = $93           ; clear screen
         bne     .loop           ; if memory page index > 0, branch to loop label
         jmp     .strt           ; repeat from start
 
-.sids   ; setup sid for noise generation
+.sids                           ; setup sid for noise generation
         lda     #$ff            ; max out
         sta     sidnsspl        ; the noise speed low SID register
         sta     sidnssph        ; the noise speed high SID register
@@ -71,8 +75,18 @@ chclrscr        = $93           ; clear screen
         sta     sidnswav        ; the waveform SID register
         rts                     ; return from subroutine
 
-.rand   ; pseudo random number generator
+.prng                           ; pseudo random number generator
         lda     sidnsval        ; load a random value from SID noise generator
         eor     rasterln        ; perform an exclusive or with current raster line
         rts                     ; register A is now holding a pseudo random number
+
+.prcg                           ; pseudo random color generator
+                                ; picks all 16 colors except black (0) and white (1)
+        jsr     .prng           ; load pseudo random number in register A
+        !for i, 1, 4 { lsr }    ; right shift 4 times, so we get a 4 bits value
+        cmp     #black          ; if black
+        beq     .prcg           ; try again
+        cmp     #white          ; if white
+        beq     .prcg           ; try again
+        rts                     ; register A is now holding a pseudo random color number
 
